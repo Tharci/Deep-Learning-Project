be6c5df30e85924c82a91537b429d352e8eb4b06
Marcell Tóth <tmarcellt.2000@gmail.com>
2022-11-07 20:02:21 +0100

    Implemented high-res input support

--- model-saves/improved-12x-RGB-v2/model.py
+++ model-saves/improved-12x-RGB-v2/model.py
@@ -1,9 +1,9 @@
-def create_improved_baseline_model_12x_comp(image_width, image_height):
+def create_improved_baseline_model_12x_comp(image_size):
     ### Encoder ###
 
     model = Sequential()
     model.add(Conv2D(16, kernel_size=(7, 7), strides=(2, 2), kernel_initializer='he_normal',
-                     padding='same', input_shape=(image_width, image_height, 3)))
+                     padding='same', input_shape=(image_size[0], image_size[1], 3)))
     model.add(BatchNormalization())
     model.add(Activation('leaky_relu'))
 
@@ -27,14 +27,14 @@ def create_improved_baseline_model_12x_comp(image_width, image_height):
     model.add(BatchNormalization())
     model.add(Activation('leaky_relu'))
 
-    model.add(Conv2D(32, kernel_size=(3, 3), strides=(2, 2), kernel_initializer='he_normal', padding='valid'))
+    model.add(Conv2D(32, kernel_size=(3, 3), strides=(2, 2), kernel_initializer='he_normal', padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('leaky_relu', name='encoded', dtype='float16'))
 
 
     ### Decoder ###
 
-    model.add(Conv2DTranspose(64, kernel_size=(3, 3), strides=(2, 2), padding='valid', kernel_initializer='he_normal'))
+    model.add(Conv2DTranspose(64, kernel_size=(3, 3), strides=(2, 2), padding='same', kernel_initializer='he_normal'))
     # model.add(BatchNormalization())
     model.add(Activation('leaky_relu'))
 
@@ -49,7 +49,7 @@ def create_improved_baseline_model_12x_comp(image_width, image_height):
     # model.add(Conv2DTranspose(16, kernel_size=(3, 3), padding='same', kernel_initializer='he_normal'))
     # model.add(BatchNormalization())
     # model.add(Activation('leaky_relu'))
-    #
+
     # model.add(Conv2DTranspose(16, kernel_size=(3, 3), padding='same', kernel_initializer='he_normal'))
     # model.add(BatchNormalization())
     # model.add(Activation('leaky_relu'))
@@ -58,7 +58,12 @@ def create_improved_baseline_model_12x_comp(image_width, image_height):
     # model.add(BatchNormalization())
     model.add(Activation('leaky_relu'))
 
-    model.add(Conv2DTranspose(3, kernel_size=(7, 7), strides=(2, 2), activation='sigmoid', padding='same', name='decoded'))
+    model.add(Conv2DTranspose(3, kernel_size=(7, 7), strides=(2, 2), activation='tanh', padding='same', name='decoded'))
+
+    print(("shape of encoded", K.int_shape(model.get_layer('encoded').output)))
+    print(("shape of decoded", K.int_shape(model.get_layer('decoded').output)))
+
+    return model
 
 
 autoencoder.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0025), loss=ssim_loss)
