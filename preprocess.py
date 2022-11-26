eda5725ff464d1e6b0aacb8204aa343da7715f01
Milosz Glowaczewski <milosz.glowaczewski@gmail.com>
2022-11-26 12:34:07 +0100

    Created depthwise model, fixed baseline model

--- preprocess.py
+++ preprocess.py
@@ -34,6 +34,37 @@ def create_dataflows(images_path, img_size, batch_size):
     return train_ds, test_ds
 
 
+def create_dataflows_grayscale(images_path, img_size, batch_size):
+    train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+        rescale=1. / 255,
+        # shear_range=0.2,
+        # zoom_range=zoom_range,
+        horizontal_flip=True,
+        validation_split=0.2,
+        # featurewise_center=True
+    )
+
+    train_ds = train_datagen.flow_from_directory(
+        images_path,
+        target_size=img_size,
+        batch_size=batch_size,
+        color_mode='grayscale',
+        class_mode=None,
+        subset='training'
+    )
+
+    test_ds = train_datagen.flow_from_directory(
+        images_path,
+        target_size=img_size,
+        batch_size=batch_size,
+        color_mode='grayscale',
+        class_mode=None,
+        subset='validation'
+    )
+
+    return train_ds, test_ds
+
+
 def crop_img(img, crop_size):
     y = np.random.randint(0, img.shape[0] - crop_size[0])
     x = np.random.randint(0, img.shape[1] - crop_size[1])
